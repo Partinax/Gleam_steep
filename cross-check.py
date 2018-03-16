890a8bf30e52f62003bc6ddee8ffc95dbab07e77
masterdefibrillator <matt_ryan2@hotmail.com>
2018-03-16 21:08:41 +0800

    changed dtype of results_table column to from O to str

--- cross-check.py
+++ cross-check.py
@@ -1,13 +1,9 @@
 from astropy.io import fits
 from optparse import OptionParser
 from astroquery.simbad import Simbad
-import argparse
 from astropy import coordinates
 import astropy.units as u
 from astroquery.skyview import SkyView
-from astropy.io.votable import parse_single_table
-from astropy.io.votable import writeto as writetoVO
-import astropy.io.votable
 from astropy.table import Table, Column
 
 usage = "Usage: %prog [options] <file>\n"
@@ -34,16 +30,19 @@ for i in cat_data:
     r = 2 * u.arcminute
     results_table = customSimbad.query_region(c, radius=r)
     image_list = SkyView.get_image_list(position=string2, survey=['NVSS', 'TGSS ADR1', 'DSS'])
-    if (type(results_table) is Table):
-        temp_field = results_table.field('OTYPE')
+    if type(results_table) is Table:
+        temp_otype = results_table.field('OTYPE')
+        results_table['MAIN_ID']=results_table['MAIN_ID'].astype(str) # convert these table columns dtypes from object to str so that they can be saved to a .fits
+        results_table['OTYPE'] = results_table['OTYPE'].astype(str)
         table_row_num = 0
         columns_removed = 0
-        for x in temp_field:
+        for x in temp_otype:
             if (x == "Star"):
-                results_table.remove(table_row_num - columns_removed)
+                results_table.remove_row(table_row_num - columns_removed)
                 columns_removed += 1
-        table_row_num += 1
-        name = str(i[0]) + '_' + str(i[2]) + '.fits'
+            table_row_num += 1
+        name = str(i[0]) + '_' + str(i[2])
         results_table.pprint()
-        results_table.write('new.fits', format='fits')
+        results_table.write('new.fits')
+    print image_list
     ob_row_num += 1
